d427c0ce805036872116d0a0fa56afff6c2c9bec
Unknown <buburgo@gmail.com>
2017-04-17 12:25:03 -0600

    Adds more accurate load data

--- PowerSources/LoadData.py
+++ PowerSources/LoadData.py
@@ -1,23 +1,22 @@
 import pandas
 
-def getInteriorLoadData():
-    with open('./Data/InteriorEquipmentLoad.txt') as loadData:
-        return [float(currentData) * 10 for currentData in loadData]
+# This is data that gives usage of interior equipment.  It may not be useful, as
+# it appears to be data for commercial buildings.
 
 def getDishwasherData():
-    with open('./Data/DishWasher1.txt') as dwData:
+    with open('./Data/Washer1.csv') as dwData:
         return [float(loadValue) for loadValue in dwData]
 
 def getMicrowaveData():
-    with open('./Data/Microwave2.txt') as mwData:
+    with open('./Data/Microwave2.csv') as mwData:
         return [float(loadValue) for loadValue in mwData]
 
 def getKettleData():
-    with open('./Data/Kettle1.txt') as ktData:
+    with open('./Data/Kettle1.csv') as ktData:
         return [float(loadValue) for loadValue in ktData]
 
 def getFridgeData():
-    with open('./Data/Fridge2.txt') as fData:
+    with open('./Data/Fridge2.csv') as fData:
         return [float(loadValue) for loadValue in fData]
 
 def getTotalLoads():
