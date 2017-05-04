db300702a451d33f4c6ebea1a5badd852a4352e3
Kevin Burgon <kevin.burgon@aggiemail.usu.edu>
2017-05-03 22:08:05 -0600

    Adds heat map generating method to evaluations file

--- evaluations.py
+++ evaluations.py
@@ -8,12 +8,18 @@ logger.setLevel(logging.CRITICAL)
 
 def getOptimalConfiguration():
 	powerSimulator = simulator()
-	return la.getBestParameters(powerSimulator.runSimulation, 2, 0, 10, 0.01)
+	results = la.getBestParameters(powerSimulator.runSimulation, 2, 0, 300, 0.01)
 
-results = getOptimalConfiguration()
-with open('results.txt', 'w') as file:
-	file.write(str(results))
+	with open('results.txt', 'w') as file:
+		file.write(str(results))
 
-print(results[1][0])
+	print(results[1][0])
 
-pd.Series(results[0]).plot()
+	pd.Series(results[0]).plot()
+
+def getConfigurationMatrix():
+	powerSimulator = simulator()
+	heatMatrix = [[powerSimulator.runSimulation([batteryCapacity / 1000, panelArea / 100]) for batteryCapacity in xrange(0, 500)] for panelArea in xrange(0, 500)]
+
+	with open('results.txt', 'w') as outFile:
+		file.write(str(heatMatrix))
